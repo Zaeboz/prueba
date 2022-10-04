274a94e32ec8695b8417833c8de938860c65689e
Zaeboz <jegs2206@gmail.com>
2022-10-04 10:21:21 -0500

    Creacion de la clase persona

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Persona p1 = new Persona("Juan", "Perez", 25, 12345678);
+        mostrarPersona(p1);
+    }
+
+    public static void mostrarPersona(Persona p) {
+        System.out.println("Nombre: " + p.getNombre());
+        System.out.println("Apellido: " + p.getApellido());
+        System.out.println("Edad: " + p.getEdad());
+        System.out.println("Telefono: " + p.getTelefono());
     }
 }
